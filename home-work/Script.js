4573b17978c4e8fe27709f94be632dc69eac3e2c
hahaz7870 <114488625+hahaz7870@users.noreply.github.com>
2023-11-25 17:06:11 +0700

    Add files via upload

---
+++ home-work/Script.js
@@ -0,0 +1,44 @@
+
+// ЗАДАНИЕ 1
+// function fill(arraySize, data) {
+//     const newArray = new Array(arraySize).fill(data);
+//     return newArray;
+//   }
+  
+//   const result = fill(3, 'a');
+//   console.log(result); // ['a', 'a', 'a']
+
+
+// ЗАДАНИЕ 2
+//   function erase(arr) {
+//     return arr.filter(item => !!item);
+//   }
+  
+//   const data = [0, 1, false, 2, undefined, '', 3, null];
+//   console.log(erase(data)); // [1, 2, 3]
+
+
+// ЗАДАНИЕ 3
+// function removeValues(arr, ...valuesToRemove) {
+//     return arr.filter(item => !valuesToRemove.includes(item));
+//   }
+  
+//   const data = [1, 2, 3, 1, 2];
+//   console.log(removeValues(data, 1, 2)); // [3]
+
+
+// ЗАДАНИЕ 4
+// function concatenate(arr, separator) {
+//     return arr.join(separator);
+//   }
+  
+//   const result = concatenate(['Я', 'Учусь', 'на', 'лучшей', 'кафедре'], ' ');
+//   console.log(result); // Я учусь на лучшей кафедре
+
+
+
+  
+
+
+  
+  
